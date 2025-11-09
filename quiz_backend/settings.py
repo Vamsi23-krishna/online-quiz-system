f6aa044b5bf4a27f98a38432b82435123d8c7d5b
Vamsi Krishna <cvamsikrishna2323@gmain.com>
2025-11-09 09:29:53 +0530

    Configured PostgreSQL database and migrations

--- quiz_backend/settings.py
+++ quiz_backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'quiz',
 ]
 
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'quiz_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quiz_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'admin123',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
     }
 }
 
